c4fc4cd226c5589d451112c76d52dcc24bc1ae89
nienzu <ibqqz0602@gmail.com>
2021-10-15 16:20:09 +0800

    新增回報管理頁面權限至各組&新增文章回報組

--- server/createGroup.py
+++ server/createGroup.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import Group, Permission
 
 seeManagerArticlePage = Permission.objects.get(name = "seeManagerArticlePage")
+seeManagerArticleReportPage = Permission.objects.get(name = "seeManagerArticleReportPage")
 articlePointManage = Permission.objects.get(name = "articlePointManage")
 
 
@@ -10,42 +11,49 @@ artistryArticleManager, created = Group.objects.get_or_create(name='美容組資
 if created:
     artistryArticleManager.permissions.add(ArtistryArticleManage)
     artistryArticleManager.permissions.add(seeManagerArticlePage)
+    artistryArticleManager.permissions.add(seeManagerArticleReportPage)
 
 NutrilliteArticleManage = Permission.objects.get(name = "NutrilliteArticleManage")
 nutrilliteArticleManager, created = Group.objects.get_or_create(name='營養組資料管理者')
 if created:
     nutrilliteArticleManager.permissions.add(seeManagerArticlePage)
     nutrilliteArticleManager.permissions.add(NutrilliteArticleManage)
+    nutrilliteArticleManager.permissions.add(seeManagerArticleReportPage)
 
 TechArticleManage = Permission.objects.get(name = "TechArticleManage")
 techArticleManager, created = Group.objects.get_or_create(name='科技組資料管理者')
 if created:
     techArticleManager.permissions.add(seeManagerArticlePage)
     techArticleManager.permissions.add(TechArticleManage)
+    techArticleManager.permissions.add(seeManagerArticleReportPage)
 
 AmwayQueenArticleManage = Permission.objects.get(name = "AmwayQueenArticleManage")
 amwayQueenArticleManager, created = Group.objects.get_or_create(name='金鍋組資料管理者')
 if created:
     amwayQueenArticleManager.permissions.add(seeManagerArticlePage)
     amwayQueenArticleManager.permissions.add(AmwayQueenArticleManage)
+    amwayQueenArticleManager.permissions.add(seeManagerArticleReportPage)
 
 OtherArticleManage = Permission.objects.get(name = "OtherArticleManage")
 otherArticleManager, created = Group.objects.get_or_create(name='其他組資料管理者')
 if created:
     otherArticleManager.permissions.add(seeManagerArticlePage)
     otherArticleManager.permissions.add(OtherArticleManage)
+    otherArticleManager.permissions.add(seeManagerArticleReportPage)
 
 ChainyenArticleManage = Permission.objects.get(name = "ChainyenArticleManage")
 chainyenArticleManager, created = Group.objects.get_or_create(name='群雁總部資料管理者')
 if created:
     chainyenArticleManager.permissions.add(seeManagerArticlePage)
     chainyenArticleManager.permissions.add(ChainyenArticleManage)
+    chainyenArticleManager.permissions.add(seeManagerArticleReportPage)
 
 SpeechArticleManage = Permission.objects.get(name = "SpeechArticleManage")
 speechArticleManager, created = Group.objects.get_or_create(name='演講廳資料管理者')
 if created:
     speechArticleManager.permissions.add(seeManagerArticlePage)
     speechArticleManager.permissions.add(SpeechArticleManage)
+    speechArticleManager.permissions.add(seeManagerArticleReportPage)
 
 
 # 文章管理大人
@@ -60,3 +68,8 @@ if created:
     articleAdminManager.permissions.add(OtherArticleManage)
     articleAdminManager.permissions.add(ChainyenArticleManage)
     articleAdminManager.permissions.add(SpeechArticleManage)
+
+#可以回報文章問題
+articleReport = Permission.objects.get(name = "articleReport")
+articleReporter, created = Group.objects.get_or_create(name='文章回報組')
+articleReporter.permissions.add(articleReport)
