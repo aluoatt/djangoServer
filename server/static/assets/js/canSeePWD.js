997b92e5492a4cdfea2a4b9f8bd8387973008c4d
DESKTOP-4A8MOPQ\aluo <aluo0511@gmail.com>
2021-10-22 11:30:20 +0800

    202110122 增加登入的眼睛

---
+++ server/static/assets/js/canSeePWD.js
@@ -0,0 +1,32 @@
+//獲取元素（兩種方式都可以）
+
+var input = document.getElementById('password');
+
+var imgs = document.getElementById('eyes');
+
+//下面是一個判斷每次點選的效果
+
+var flag = 0;
+
+imgs.onclick = function () {
+
+    if (flag == 0) {
+
+        input.type = 'text';
+
+        eyes.src = '/static/images/pwdOpenEye.png';//睜眼圖
+
+        flag = 1;
+
+    } else {
+
+        input.type = 'password';
+
+        eyes.src = '/static/images/pwdCloseEye.png';//閉眼圖
+
+        flag = 0;
+
+    }
+
+}
+
